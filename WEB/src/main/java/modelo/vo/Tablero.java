d5725e718ca033bb000df3cf11d29a6da884e8b8
Ciro Fustero <900506@unizar.es>
2026-02-21 16:36:00 +0100

    ordenacion de codigo

---
+++ WEB/src/main/java/modelo/vo/Tablero.java
@@ -0,0 +1,31 @@
+package modelo.vo;
+
+public class Tablero {
+    public Posicion[][] tablero;
+    public Posicion trono1, trono2;
+
+    public Tablero(int DIM){
+        if(DIM%2!=0){
+            tablero = new Posicion[DIM][DIM];
+            for (int i=0; i<DIM; i++) {
+                for(int j=0; j<DIM; j++) {
+                    if (i == 0) {
+                        tablero[i][j] = new Posicion(j, i, new Ficha(false, 1)); //Codigo peon
+                        tablero[i][j] = new Posicion(j, i, new Ficha(true, 1)); //Codigo rey
+                    }else if (i == DIM - 1) {
+                        tablero[i][j] = new Posicion(j, i, new Ficha(false, 2)); 
+                        tablero[i][j] = new Posicion(j, i, new Ficha(true, 2)); 
+                    }else{
+                        tablero[i][j] = new Posicion(j, i, null);
+                    }
+                }
+            }
+            trono1 = new Posicion(DIM/2, 0, null);
+            trono2 = new Posicion(DIM/2, DIM-1, null);
+        }
+    }
+    
+    public Posicion getPosicion(int x, int y){
+    	return tablero[y][x];
+    }
+}
