10d6d340bc1e9783a137c255ee4469ee11f6ca7b
Ciro Fustero <900506@unizar.es>
2026-02-21 16:06:22 +0100

    Desarrollo de las clase PartidaJDBC, arreglo en la BD y clases CartaMovJDBC y CartaAccionJDBC, y añadimos el script para borrar las tablas de la BD para actualizarlas

--- BaseDatos/CartasAccionJDBC.java
+++ BaseDatos/CartasAccionJDBC.java
@@ -5,6 +5,7 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public final class CartasAccionJDBC implements movimientoDAO {
 
@@ -93,6 +94,73 @@ public final class CartasAccionJDBC implements movimientoDAO {
             }
         }
         return cartas;
+    }
+    
+    public List<CartaAccion> sacarCartasPartida(int IDPartida) throws SQLException {
+        final String sql = "SELECT c.Nombre, c.Accion, c.Puntos_min FROM Cartas_Accion c, Partida_Cartas_Accion p WHERE c.Nombre = ID_Carta_Accion AND ID_Partida = ?";
+
+        List<CartaAccion> cartas = new ArrayList<>();
+
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement p = conn.prepareStatement(sql)) {
+            p.setInt(2, IDPartida); 
+
+            try (ResultSet rs = p.executeQuery()) {
+                while (rs.next()) {
+                    cartas.add(montarAccion(rs));
+                }
+            }
+        }
+        return cartas;
+    }
+
+    public boolean updateEstadoEnPartida(int IDPartida, String nombreCarta, String estado) throws SQLException {
+        try(Connection c = dataSource.getConnection(); 
+            PreparedStatement p = c.prepareStatement("UPDATE Partida_Cartas_Accion SET Estado = ? WHERE ID_Carta_Accion = ? AND ID_Partida = ?")) { 
+            p.setString(1, estado); 
+            p.setString(2, nombreCarta); 
+            p.setInt(2, IDPartida); 
+            p.executeUpdate(); 
+            return true;
+        }catch (SQLException e) {
+            return false; // Si hay una excepción, asumimos que no se creó
+        }
+    }
+
+    public boolean asignar4CartasPartida(int IDPartida, int puntosMin) {
+        List<CartaAccion> disponibles = sacarCartas();
+
+        //Eliminamos de la lista las cartas que sobrepasen el minimo IMPORTANTE: Poner al menos 4 cartas que no necesiten un minimos de puntos para jugarse
+        for(CartaAccion carta : disponibles){
+            if(carta.getPuntosMin() > puntosMin){
+                disponibles.remove(carta);
+            }
+        }
+
+        Collections.shuffle(disponibles);
+        for(int i = 0; i<4; i++){
+            if(!asignarCartaPartida(IDPartida, disponibles.get(i).getNombre())){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean asignarCartaPartida(int IDPartida, String nombreCarta) throws SQLException {
+        final String sql = "INSERT INTO Partida_Cartas_Accion (ID_Partida, ID_Carta_Accion, Estado) VALUES (?, ?, ?)";
+        
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)) {
+            
+            ps.setInt(1, IDPartida);
+            ps.setString(2, nombreCarta);
+            ps.setString(3, "Mazo");
+            int filasAfectadas = ps.executeUpdate();
+            return filasAfectadas > 0;
+            
+        } catch (SQLException e) {
+            return false; // Si hay una excepción, asumimos que no se creó
+        }
     }
 
     public void borrar(String nombre) throws SQLException {
---
+++ BaseDatos/borrar.sql
@@ -0,0 +1,14 @@
+--Borrar tablas para modificaciones en BD
+
+DROP INDEX solo1J1;
+DROP INDEX solo1J2;
+
+DROP TABLE Partida_Cartas_Accion;
+DROP TABLE Partida_Cartas_Mov;
+DROP TABLE Amistades;
+DROP TABLE Jugador_Skins;
+DROP TABLE Partida;
+DROP TABLE Cartas_Accion;
+DROP TABLE Cartas_Mov;
+DROP TABLE Skin;
+DROP TABLE Jugador;
