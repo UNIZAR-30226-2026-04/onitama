d0664581afcda5e6ec3d46aea4a4530f5211727c
Ciro Fustero <900506@unizar.es>
2026-02-25 21:35:11 +0100

    Cartas del juego

--- BaseDatos/construir.sql
+++ BaseDatos/construir.sql
@@ -14,3 +14,13 @@ INSERT INTO Cartas_Mov (Nombre, Movimientos) VALUES ('Grulla', '(1,-1),(-1,-1),(
 INSERT INTO Cartas_Mov (Nombre, Movimientos) VALUES ('Oso', '(1,0),(-1,0),(0,1)');
 INSERT INTO Cartas_Mov (Nombre, Movimientos) VALUES ('Aguila', '(-1,0),(-1,1),(-1,-1)');
 INSERT INTO Cartas_Mov (Nombre, Movimientos) VALUES ('Cobra', '(-1,0),(1,1),(1,-1)');
+
+INSERT INTO Cartas_Accion (Nombre, Accion, Puntos_min) VALUES ('Pensatorium', 'ESPEJO', 0);
+INSERT INTO Cartas_Accion (Nombre, Accion, Puntos_min) VALUES ('Santo Grial', 'REVIVIR', 0);
+INSERT INTO Cartas_Accion (Nombre, Accion, Puntos_min) VALUES ('Illusia', 'SALVAR_REY', 500);
+INSERT INTO Cartas_Accion (Nombre, Accion, Puntos_min) VALUES ('Requiem', 'SACRIFICIO', 1000); --Se sacrifica un peon tuyo para matar a otro del enemigo
+INSERT INTO Cartas_Accion (Nombre, Accion, Puntos_min) VALUES ('La Dama del Mar', 'SOLO_PARA_ADELANTE', 0);
+INSERT INTO Cartas_Accion (Nombre, Accion, Puntos_min) VALUES ('Kelpie', 'VENGANZA', 5000); --Si te matan al rey antes de los primeros 5 minutos, puedes jugar esta carta para quedar empate
+INSERT INTO Cartas_Accion (Nombre, Accion, Puntos_min) VALUES ('Atrapasueños', 'ROBAR', 1000);
+INSERT INTO Cartas_Accion (Nombre, Accion, Puntos_min) VALUES ('Brujeria', 'CEGAR', 0);
+INSERT INTO Cartas_Accion (Nombre, Accion, Puntos_min) VALUES ('Finisterra', 'SOLO_PARA_ATRAS', 0);
