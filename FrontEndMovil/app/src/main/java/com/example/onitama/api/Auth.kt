fe443959941d9258eeaa8cf13b643120772494c2
Freakmaster2000 <898030@unizar.es>
2026-05-13 11:32:58 +0200

    Mejorado feedback al usuario movil al iniciar sesion y registrarse

--- FrontEndMovil/app/src/main/java/com/example/onitama/api/Auth.kt
+++ FrontEndMovil/app/src/main/java/com/example/onitama/api/Auth.kt
@@ -218,7 +218,7 @@ class Auth(
             // con el registro lanza una excepción.
             val respuesta = jsonSerializer.decodeFromString<MensajeServidor>(respuestaStr)
             if (respuesta is MensajeRegistroErroneo) {
-                throw Exception("Error al registrar el usuario, ese nombre ya existe")
+                throw Exception("Error al registrar, ya existe alguien con ese nombre")
             }
         } catch (e: Exception) {
             Log.e("Auth_API", "Error al registrar usuario", e)
